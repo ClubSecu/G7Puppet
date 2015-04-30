2222197b20eb66b8e8c8cdf12b4eef9cd7723609
Group7 <root@localhost.localdomain>
2015-04-30 10:48:08 +0200

    Starting configuring Apache : Modifying g7apache/manifests/params.pp with adding variable for default error web page, adding owner variable to osfamily

--- g7apache/manifests/params.pp
+++ g7apache/manifests/params.pp
@@ -1,6 +1,14 @@
 class g7apache::params{
 	case $::osfamily{
-		Debian:{$apache='apache2'}
-		CentOs:{$apache='httpd'}
-	}		
+		Debian:{
+			$apache='apache2', #service name
+			$owner='www-data', #service account name
+		}
+		CentOs:{
+			$apache='httpd',  #service name
+			$owner='apache',  #service account name
+		}
+	}	
+	$noindex="../files/noindex.html"  # link error web page 
+		
 }
