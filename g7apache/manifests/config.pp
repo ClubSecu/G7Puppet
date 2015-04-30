9a8357ce6180dd5df6c6810993f09ce2b1682d57
Group7 <root@localhost.localdomain>
2015-04-30 12:54:10 +0200

    Starting configuring Apache : Patching g7apache/manifests/params.pp and g7apache/manifests/config.pp - Resolving syntaxe error

--- g7apache/manifests/config.pp
+++ g7apache/manifests/config.pp
@@ -1,15 +1,15 @@
 class g7apache::config {
 	file { 'ERROR - Directory':
-		path => "${g7apache::params:www_path}/error", 
+		path => "${g7apache::params::www_path}error", 
 		ensure => directory, 
-		owner => $g7apache::params:owner, 
+		owner => $g7apache::params::owner, 
 	}
 	#default page config:
-	file { 'ERROR - File'      
-		path => "${g7apache::params:www_path}/error/noindex.html",
+	file { 'ERROR - File':      
+		path => "${g7apache::params::www_path}error/noindex.html",
 		source => $g7apache::params::noindex,
 		mode => 0644, 
 		ensure => file,
-		owner => $g7apache::params:owner, 
+		owner => $g7apache::params::owner, 
 	}
 }
--- g7apache/manifests/params.pp
+++ g7apache/manifests/params.pp
@@ -1,14 +1,14 @@
-class g7apache::params{
-	case $::osfamily{
+class g7apache::params {
+	case $::osfamily {
 		Debian: {
-			$package_name='apache2', #service name 
-			$owner='www-data', #service account name
-			$www_path='/var/www/', # path to web directory
+			$package_name='apache2'  #service name
+			$owner='www-data' #service account name
+			$www_path='/var/www/' # path to web directory
 		}
 		CentOs: {
-			$package_name='httpd',  #service name
-			$owner='apache',  #service account name
-			$www_path='/var/www/html/', # path to web directory
+			$package_name='httpd'  #service name
+			$owner='apache'  #service account name
+			$www_path='/var/www/html/' # path to web directory
 		}
 	}	
 	$noindex="../files/noindex.html"  # link error web page 
