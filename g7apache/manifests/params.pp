353a228b8c00d7ed2151d020faa09d9c2a95169b
Group7 <root@localhost.localdomain>
2015-04-30 12:38:41 +0200

    Starting configuring Apache : Patching g7apache/manifests/params.pp - Changing name of variable apache to package

--- g7apache/manifests/params.pp
+++ g7apache/manifests/params.pp
@@ -1,12 +1,12 @@
 class g7apache::params{
 	case $::osfamily{
-		Debian:{
-			$apache='apache2', #service name
+		Debian: {
+			$package='apache2', #service name 
 			$owner='www-data', #service account name
 			$www_path='/var/www/', # path to web directory
 		}
-		CentOs:{
-			$apache='httpd',  #service name
+		CentOs: {
+			$package='httpd',  #service name
 			$owner='apache',  #service account name
 			$www_path='/var/www/html/', # path to web directory
 		}
