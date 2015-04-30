d0b9b6a5d9593df943b0a735432ee0721853c757
Group7 <root@localhost.localdomain>
2015-04-30 10:40:40 +0200

    Starting configuring Apache : Set-up g7apache/manifests/config.pp with default web page for /var/www

--- g7apache/manifests/config.pp
+++ g7apache/manifests/config.pp
@@ -1,3 +1,11 @@
 class g7apache::config {
-	#todo if necessary
+	file { 'error':
+		path => '/var/www/error', 
+		ensure => directory, 
+	}
+	file { '/var/www/noindex.html' : 
+		source => $g7apache::params::default_web_page,
+		mode => 0644, 
+		ensure => file, 
+	}
 }
