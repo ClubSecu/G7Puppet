662574141681ded215c99a5bf32d543206573749
fkleinbourg <fabien@kleinbourg.fr>
2015-04-30 09:24:54 +0000

    changed mysql params. all mysql package have the same name on all os family

--- mysql/manifests/params.pp
+++ mysql/manifests/params.pp
@@ -1,17 +1,4 @@
 class mysql::params {
-	case $::osfamily {
-		Debian: {
-			$package_name = mysql-server
-			$service_name = mysqld
-		}
-		
-		RedHat: {
-			$package_name = mysql-server
-			$service_name = mysqld
-		}
-	
-		default: {
-			fail("The ${module_name} module is not supported on ${::operatingsystem}")
-		}
-	}
+	$package_name = mysql-server
+	$service_name = mysql
 }
