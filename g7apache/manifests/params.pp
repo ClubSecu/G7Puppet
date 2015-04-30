93470fb8f9a367a4a9688cfca93234dd79d55309
Group7 <root@localhost.localdomain>
2015-04-30 12:10:29 +0200

    Starting configuring Apache : Modifying g7apache/manifests/params.pp with adding variable for path to web directory which is different for each os

--- g7apache/manifests/params.pp
+++ g7apache/manifests/params.pp
@@ -3,10 +3,12 @@ class g7apache::params{
 		Debian:{
 			$apache='apache2', #service name
 			$owner='www-data', #service account name
+			$www_path='/var/www/', # path to web directory
 		}
 		CentOs:{
 			$apache='httpd',  #service name
 			$owner='apache',  #service account name
+			$www_path='/var/www/html', # path to web directory
 		}
 	}	
 	$noindex="../files/noindex.html"  # link error web page 
