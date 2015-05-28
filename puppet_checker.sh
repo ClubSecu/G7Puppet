2f387d4b0cc3c66a063dc8b5de440161db181a76
fkleinbourg <fabien@kleinbourg.fr>
2015-05-28 11:00:34 +0000

    Puppet_Checker : Updated and added action function

--- puppet_checker.sh
+++ puppet_checker.sh
@@ -1,26 +1,30 @@
 #!/bin/bash -eu
-
+txtgreen="\e[32m"
+txtred="\e[31m"
+txtnormal="\e[0m"
 usage(){
     echo -e "\
     Usage : $0 module_name...
     This script validates the puppet module"
 }
 
+action(){
+    if $1
+    then
+        printf "%*b\r%s\n" "$(($(tput cols)+9))" "[${txtgreen}    OK    ${txtnormal}]" "$2"
+    else
+        printf "%*b\r%s\n" "$(($(tput cols)+9))" "[${txtred}   FAIL   ${txtnormal}]" "$2"
+    fi
+}
+
 [ $# -ne 1 ] \
     && ( usage && exit 1 )\
     || module=$1
 
-echo -e "Checking if directory exists..."
-[ -d $module ] \
-    && echo -e "OK !" \
-    || ( echo -e "KO !" && exit 1 )
-
-echo -e "Checking parse for $module"
-puppet parser validate $PWD/$module/manifests/* \
-    && echo -e "Parse OK !" \
-    || ( echo -e "Parse KO !" && exit 1 )
-
-echo -e "Trying a puppet apply !"
-puppet apply --modulepath=$PWD -e "include $module" --noop \
-    && echo -e "module OK !" \
-    || ( echo -e "module KO !" && exit 1 )
+action "puppet parser validate $PWD/$module/manifests/*" "Checking parse for $module"
+if puppet apply --modulepath=$PWD -e "include $module" --noop 
+then
+    printf "%*b\r%s\n" "$(($(tput cols)+9))" "[${txtgreen}    OK    ${txtnormal}]" "Trying a puppet apply !"
+else
+    printf "%*b\r%s\n" "$(($(tput cols)+9))" "[${txtred}   FAIL   ${txtnormal}]" "Trying a puppet apply !"
+fi
