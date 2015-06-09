a2510a85e916b8c4e3ff6634f85bc560348f7171
Group7 <root@localhost.localdomain>
2015-06-09 15:40:35 +0200

    Modifying module Muninsrv : Modifying g7muninsrv/manifests/init.pp - Removing useless comments and adding references to other files

--- g7muninsrv/manifests/init.pp
+++ g7muninsrv/manifests/init.pp
@@ -1,41 +1,5 @@
 # == Class: g7muninsrv
-#
-# Full description of class g7muninsrv here.
-#
-# === Parameters
-#
-# Document parameters here.
-#
-# [*sample_parameter*]
-#   Explanation of what this parameter affects and what it defaults to.
-#   e.g. "Specify one or more upstream ntp servers as an array."
-#
-# === Variables
-#
-# Here you should define a list of variables that this module would require.
-#
-# [*sample_variable*]
-#   Explanation of how this variable affects the funtion of this class and if it
-#   has a default. e.g. "The parameter enc_ntp_servers must be set by the
-#   External Node Classifier as a comma separated list of hostnames." (Note,
-#   global variables should not be used in preference to class parameters  as of
-#   Puppet 2.6.)
-#
-# === Examples
-#
-#  class { g7muninsrv:
-#    servers => [ 'pool.ntp.org', 'ntp.local.company.com' ]
-#  }
-#
-# === Authors
-#
-# Author Name <author@domain.com>
-#
-# === Copyright
-#
-# Copyright 2011 Your name here, unless otherwise noted.
-#
 class g7muninsrv {
-
-
+	Class['g7muninsrv::params']~>Class['g7muninsrv::install']~>Class['g7muninsrv::service']~>Class['g7muninsrv::config']
+	include g7muninsrv::params, g7muninsrv::install, g7muninsrv::service, g7muninsrv::config
 }
