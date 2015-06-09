d3309bbefbc0ebb902d5d2f9892d1b64911e67fd
Group7 <root@localhost.localdomain>
2015-06-09 17:02:46 +0200

    Modifying module g7muninsrv : Patching g7muninsrv/manifests/params.pp - Modifying package name from munin-plugins, which was a meta-package, to munin-plugins-core, a real package

--- g7muninsrv/manifests/params.pp
+++ g7muninsrv/manifests/params.pp
@@ -1,4 +1,4 @@
 class g7muninsrv::params {
 	$package_srv='munin'  #service name for munin server
-	$package_plugins='munin-plugins' # service name for munin plugins
+	$package_plugins='munin-plugins-core' # service name for munin plugins
 }
