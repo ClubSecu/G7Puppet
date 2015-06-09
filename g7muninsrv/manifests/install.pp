96dbf79ce430cedf160b2bbcb06a88f323412783
Group7 <root@localhost.localdomain>
2015-06-09 16:33:18 +0200

    Starting configuring Muninsrv : Adding apache module rquirement in g7muninsrv/manifests/install.pp

--- g7muninsrv/manifests/install.pp
+++ g7muninsrv/manifests/install.pp
@@ -1,4 +1,7 @@
 class g7muninsrv::install {
+	# Installing module apache which is required in munin
+	require g7apache
+	
 	package { $g7muninsrv::params::package_name : 
 		ensure => installed,
 	}
