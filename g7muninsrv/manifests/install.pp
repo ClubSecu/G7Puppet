2aa457a6be3e059f1cd66e86c9c57e22a59ab6d5
Group7 <root@localhost.localdomain>
2015-06-09 16:54:11 +0200

    Modifying module g7muninsrv : Patching g7muninsrv/manifests/install.pp - Changing name of variable package from package_name to package_srv and adding new package to install which is munin-plugins

--- g7muninsrv/manifests/install.pp
+++ g7muninsrv/manifests/install.pp
@@ -2,7 +2,10 @@ class g7muninsrv::install {
 	# Installing module apache which is required in munin
 	require g7apache
 	
-	package { $g7muninsrv::params::package_name : 
+	package { $g7muninsrv::params::package_srv : 
+		ensure => installed,
+	}
+	package { $g7muninsrv::params::package_plugins : 
 		ensure => installed,
 	}
 
