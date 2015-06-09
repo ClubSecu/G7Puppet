5c3be375d95243b3cc977558daf193c45d8af5c9
Group7 <root@localhost.localdomain>
2015-06-09 18:52:05 +0200

    Modifying module g7muninsrv : Adding configuration for template generation in g7muninsrv/manifests/config.pp

--- g7muninsrv/manifests/config.pp
+++ g7muninsrv/manifests/config.pp
@@ -1,3 +1,12 @@
 class g7muninsrv::config {
 	notice("55 55 44 44 4C 52 4C 52 42 41 - Contra")
+
+	#Munin server configuration file which automatically add node's configuration
+	file { "g7muninsrv::params::conf_file" :
+		path => "${g7muninsrv::params::conf_path}munin.conf",
+		content => template("g7muninsrv/munin.conf.erb"),
+		mode => 0644,
+		ensure => file,
+		owner => root,
+	}
 }
