1634dcb297bcc7467f23296c58d02941281ac20b
Group7 <root@localhost.localdomain>
2015-06-09 18:16:50 +0200

    Modifying module g7muninsrv : Removing useless configuration variables to use a template instead for munin server configuration in g7muninsrv/manifests/params.pp

--- g7muninsrv/manifests/params.pp
+++ g7muninsrv/manifests/params.pp
@@ -3,8 +3,5 @@ class g7muninsrv::params {
 	$package_plugins='munin-plugins-core' # service name for munin plugins
 
 	#Auto-configuration variables
-	$conf_file='/etc/munin/munin-node.conf'
-	$conf_node_name = "fabien"
-	$conf_node_address = "192.168.0.2"
-	$conf_node_params = "yes"
+	$conf_file='/etc/munin/munin-conf.d/'
 }
